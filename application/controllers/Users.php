0812c5635e94d4e9bc45f6373c9593cd29e321ff
Furkan Çeker <ceker0901@gmail.com>
2023-03-04 15:35:52 +0300

    framework settings

---
+++ application/controllers/Users.php
@@ -0,0 +1,11 @@
+<?php
+
+class Users extends CI_Controller {
+    public function __construct(){
+        parent::__construct();
+    }
+
+    public function index(){
+        echo "İndex";
+    }
+}
